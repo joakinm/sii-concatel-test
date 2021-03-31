7cbea6de86e309cca2104cd19475d57ebd36dce0
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-31 11:51:37 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FizzbuzzTestComponent } from './fizzbuzz-test/fizzbuzz-test.component';
+import { FormsModule } from '@angular/forms';
+import { NasaTestComponent } from './nasa-test/nasa-test.component';
+import { EmpireTestComponent } from './empire-test/empire-test.component';
+import { TestSelectorComponent } from './test-selector/test-selector.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FizzbuzzTestComponent,
+    NasaTestComponent,
+    EmpireTestComponent,
+    TestSelectorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
