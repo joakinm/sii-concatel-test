bf282e0e216b8e16ec9520b769303c6130b97e2e
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-31 13:42:32 +0200

    nasa-test created

---
+++ src/app/nasa-test/nasa-test.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-nasa-test',
+  templateUrl: './nasa-test.component.html',
+  styleUrls: ['./nasa-test.component.css']
+})
+export class NasaTestComponent implements OnInit {
+  @ViewChild("f") form: NgForm;
+  public number: number;
+  public count: number;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  public testNasa() {
+    this.count = this.number;
+    let intervalId = setInterval(() => {
+      this.count = this.count - 1;
+        console.log(this.count)
+        if(this.count === 0) {clearInterval(intervalId)}
+    }, 1000)
+  }
+}
+  
